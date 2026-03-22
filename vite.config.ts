1d1219e6f998e48f16c942601e1e4d9ede352b56
HaoHoa <tanhao1852000@gmail.com>
2026-03-22 18:11:50 +0700

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,14 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://103.173.226.3:8080',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
