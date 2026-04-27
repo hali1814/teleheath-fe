fac6b0066117c6d400b61e775a14087f7d81e85f
Nhat Dang <nhatdzfbk@gmail.com>
2026-04-28 02:28:25 +0700

    feat: new ui for service

--- src/sections/book-appointment/ServiceCard.tsx
+++ src/sections/book-appointment/ServiceCard.tsx
@@ -33,7 +33,7 @@ export const ServiceCard = ({
   return (
     <div
       className={cn(
-        'relative flex flex-col gap-[12px] px-[16px] py-[12px] rounded-[12px] bg-white border border-[#D331311A]',
+        'relative w-[180px] flex flex-col gap-[12px] rounded-[12px] bg-white border border-[#E2E2E2]',
         selected && 'border-[#ED2630] shadow-[0_0_0_1px_#ED2630]',
         disabled &&
           'opacity-50 pointer-events-none cursor-not-allowed select-none',
@@ -43,135 +43,94 @@ export const ServiceCard = ({
       aria-disabled={disabled}
       onClick={() => {
         if (disabled) return
-        onClick()
+        onDetailClick()
       }}
     >
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-[12px]">
-          <Avatar>
-            <AvatarImage
-              src={service.partner.photoUrl}
-              alt={service.partner.name}
-            />
-          </Avatar>
-          <Text
-            size="xs_10"
-            className="font-extrabold leading-[1.2] text-muted-foreground"
-          >
-            {service.partner.name}
-          </Text>
+      <Image
+        src={service.partner.photoUrl}
+        alt={service.partner.name}
+        className="aspect-3/2 object-cover rounded-tl-[12px] rounded-se-[12px]"
+      />
+      <div className="flex flex-col gap-[8px] p-[12px]">
+        <Text size="sm_12" className="leading-[14px] font-semibold">
+          {typeName}
+        </Text>
+        <div className="flex items-center flex-wrap gap-[8px]">
+          {amenities &&
+            amenities.map((amenity: Amenity) => (
+              <Image
+                src={amenity.iconUrl}
+                alt={amenity.name}
+                className="size-[12px] object-contain"
+              />
+            ))}
         </div>
-        <button
-          type="button"
-          className="flex items-center gap-[4px] px-[8px] py-[6px] rounded-[6px] bg-dust-red-1"
-          onClick={(e) => {
-            e.stopPropagation()
-            onDetailClick()
-          }}
-        >
-          <Icon name="eye_outline" className="w-[16px] h-[16px] text-primary" />
-          <Text size="sm_12" className="leading-[1.3] font-medium text-primary">
-            Details
-          </Text>
-        </button>
-      </div>
-      <Text size="sm_12" className="leading-[1.2] font-medium">
-        {typeName}
-      </Text>
-      {price !== 0 && price && (
-        <div className="flex justify-between items-center">
-          <Text
-            size="xs_10"
-            className="leading-[15px] tracking-[1px] text-[#707977]"
-          >
-            Price around
-          </Text>
-          {hasActiveDiscount ? (
-            <div className="flex items-center gap-[8px]">
+        {price !== 0 && price && (
+          <div className="flex flex-col">
+            <Text size="xs_10" className="leading-[14px] text-[#475569]">
+              Reference price
+            </Text>
+            {hasActiveDiscount ? (
+              <div className="flex flex-wrap items-center gap-x-[8px] gap-y-[4px]">
+                <Text
+                  size="lg_16"
+                  className="leading-[22px] font-medium text-[#EC5B13]"
+                >
+                  {formatPrice(promotionPrice)}
+                </Text>
+                <Text
+                  size="lg_16"
+                  className="leading-[22px] font-medium text-muted-foreground line-through"
+                >
+                  {originalPrice !== 0 && originalPrice
+                    ? formatPrice(originalPrice)
+                    : 'Contact later'}
+                </Text>
+              </div>
+            ) : (
               <Text
-                size="sm_12"
-                className="leading-[1.2] font-extrabold text-[#E22A36]"
-              >
-                {formatPrice(promotionPrice)}
-              </Text>
-              <Text
-                size="sm_12"
-                className="leading-[1.2] font-extrabold text-muted-foreground line-through"
+                size="lg_16"
+                className="leading-[22px] font-medium text-[#EC5B13]"
               >
                 {originalPrice !== 0 && originalPrice
                   ? formatPrice(originalPrice)
                   : 'Contact later'}
               </Text>
-            </div>
-          ) : (
+            )}
+          </div>
+        )}
+        {service.partner.distanceFromHospital && (
+          <div className="flex flex-col">
+            <Text size="xs_10" className="leading-[14px] text-[#475569]">
+              *Distance from hospital
+            </Text>
             <Text
               size="sm_12"
-              className="leading-[1.2] font-extrabold text-[#00322D]"
+              className="leading-[1.2] font-medium text-muted-foreground"
             >
-              {originalPrice !== 0 && originalPrice
-                ? formatPrice(originalPrice)
-                : 'Contact later'}
+              {service.partner.distanceFromHospital}
             </Text>
-          )}
-        </div>
-      )}
-      {service.partner.distanceFromHospital && (
-        <div className="flex justify-between items-center">
-          <Text
-            size="xs_10"
-            className="leading-[15px] tracking-[1px] text-[#707977]"
-          >
-            Distance from hospital
+          </div>
+        )}
+        <button
+          className="w-full h-[28px] bg-dust-red-1 rounded-[6px]"
+          onClick={(e) => {
+            e.stopPropagation()
+            onClick()
+          }}
+        >
+          <Text size="sm_12" className="leading-[1.3] font-medium text-primary">
+            Select
           </Text>
-          <Text
-            size="sm_12"
-            className="leading-[1.2] font-extrabold text-[#00322D]"
-          >
-            {service.partner.distanceFromHospital}
-          </Text>
-        </div>
-      )}
-      <div className="flex items-center flex-wrap gap-[12px]">
-        {amenities &&
-          amenities.map((amenity: Amenity) => (
-            <div
-              key={amenity.name}
-              className="flex items-center gap-[6px] bg-[#EDEEED] px-[12px] py-[4px] rounded-full"
-            >
-              <Image
-                src={amenity.iconUrl}
-                alt={amenity.name}
-                className="w-[16px] h-[16px] object-contain"
-              />
-              <Text
-                size="xs_10"
-                className="leading-[16px] font-medium text-[#3F4947]"
-              >
-                {amenity.name}
-              </Text>
-            </div>
-          ))}
-      </div>
-      <div className="flex justify-end">
-        <LocationBadge
-          location={getLocalizedTextByLang(
-            service.partner.country[0].nameVi,
-            null,
-            service.partner.country[0].nameEn,
-            i18n.language as AppLanguage,
-          )}
-          textSize="xs_10"
-          className="leading-[1.3] text-muted-foreground"
-          iconSize="w-[14px] h-[14px]"
-        />
+        </button>
       </div>
       {isBest && (
         <div className="absolute -top-2.5 left-0">
-          <Image
+          {/* <Image
             src="/best-service.svg"
             alt="Best service"
             className="h-[23px] object-contain"
-          />
+          /> */}
         </div>
       )}
       {selected && (
