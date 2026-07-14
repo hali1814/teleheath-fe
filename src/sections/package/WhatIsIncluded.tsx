3196a821a46f5f2b7d3c1647763b7ed6df0bdc1f
hodacdat <dat.ho@altek.com.vn>
2026-07-14 09:10:05 +0700

    update: update package UI in user app: show description of package below the package name

--- src/sections/package/WhatIsIncluded.tsx
+++ src/sections/package/WhatIsIncluded.tsx
@@ -1,18 +1,24 @@
 import { Icon } from '#/components/icon'
 import Text from '#/components/text'
 import { useTranslation } from 'react-i18next'
+import type { CheckupTypeEntity } from '#/entities/packageEntity'
 
-const IncludedItem = ({ name }: { name: string }) => {
+const IncludedItem = ({ item }: { item: CheckupTypeEntity }) => {
   return (
-    <div className="flex items-center gap-[16px]">
+    <div className="flex items-start gap-[16px]">
       <Icon
         name="check_circle_outline"
-        className="w-[16px] h-[16px] text-primary"
+        className="w-[16px] h-[16px] text-primary mt-1"
       />
-      <div className="flex-1 flex flex-col gap-[6px]">
+      <div className="flex-1 flex flex-col gap-[4px]">
         <Text className="font-medium leading-normal text-[#333333]">
-          {name}
+          {item.name}
         </Text>
+        {item.description && (
+          <Text size="sm_12" className="text-[#666666] whitespace-pre-line leading-relaxed">
+            {item.description}
+          </Text>
+        )}
       </div>
     </div>
   )
@@ -21,7 +27,7 @@ const IncludedItem = ({ name }: { name: string }) => {
 export default function WhatIsIncluded({
   checkupTypes,
 }: {
-  checkupTypes: string[]
+  checkupTypes: CheckupTypeEntity[]
 }) {
   const { t } = useTranslation(['package'])
 
@@ -31,7 +37,7 @@ export default function WhatIsIncluded({
         {t('whatIsIncluded')}
       </Text>
       {checkupTypes.map((item, index) => (
-        <IncludedItem key={index} name={item} />
+        <IncludedItem key={index} item={item} />
       ))}
     </div>
   )
